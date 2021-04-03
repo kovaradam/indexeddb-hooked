6fc5fb141e16ee55493a60e44442f992ee44eaca
Adam Kovář <kovaradam1@gmail.com>
2021-04-03 10:14:45 +0200

    :construction_worker: update build

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,22 @@ import dts from 'rollup-plugin-dts';
 import pkg from './package.json';
 import tsconfig from './tsconfig.json';
 
+const tsOutDir = tsconfig.compilerOptions.outDir;
+
 export default [
   {
-    input: `${tsconfig.compilerOptions.outDir}/index.js`,
+    input: [
+      `${tsOutDir}/index.js`,
+      `${tsOutDir}/hooks/use-read.js`,
+      `${tsOutDir}/hooks/use-update.js`,
+      `${tsOutDir}/operators/read.js`,
+      `${tsOutDir}/operators/update.js`,
+    ],
     output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' },
+      // { file: pkg.main, format: 'cjs' },
+      // { file: pkg.module, format: 'es' },
+      { dir: 'lib/esm', format: 'es' },
+      { dir: 'lib/cjs', format: 'cjs', exports: 'named' },
     ],
     external: ['react'],
     onwarn: function (warning) {
@@ -19,7 +29,7 @@ export default [
     },
   },
   {
-    input: `${tsconfig.compilerOptions.outDir}/index.js`,
+    input: `${tsOutDir}/index.js`,
     output: {
       file: pkg.types,
     },
