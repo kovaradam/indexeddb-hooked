fed15746fa3fe93d781cc9a3cf31aee6fe4cea3b
Adam Kovář <kovaradam1@gmail.com>
2021-04-12 16:54:36 +0200

    :recycle: refactor useRead test

--- tests/api/use-read.test.tsx
+++ tests/api/use-read.test.tsx
@@ -13,19 +13,28 @@ import {
 import Store from '../../src/store';
 import { keyPath, keyPath2, sleep, stores } from '../utils';
 
-const [errorMsg, nullMsg] = ['error', 'loading'];
+async function renderHook(storeName: string, params?: ReadParams, time = 10) {
+  let result = { current: null };
 
-const ReadComponent: React.FC<{
-  storeName: string;
-  params?: ReadParams;
-  transform?: (result: ReadResult<DBRecord>) => any;
-}> = ({ storeName, params, transform }) => {
-  const [result, error] = useRead(storeName, params);
-  transform ||= (result) => JSON.stringify(result);
-  if (error) return <div>{errorMsg}</div>;
-  if (!result) return <div>{nullMsg}</div>;
-  return <div>{transform(result)}</div>;
-};
+  function TestComponent() {
+    result.current = useRead(storeName, params);
+    return null;
+  }
+
+  await act(async () => {
+    render(<TestComponent />, container);
+    await sleep(time);
+  });
+
+  return result;
+}
+
+function createResult(
+  result: ReadResult<DBRecord>,
+  error?: string,
+): [ReadResult<DBRecord>, string?] {
+  return [result, error];
+}
 
 beforeAll((done) => {
   const config: Config = {
@@ -48,27 +57,12 @@ afterEach(() => {
   container = null;
 });
 
-function _render(component: React.ReactElement) {
-  render(component, container);
-}
-
-async function renderAndWait(component: React.ReactElement, time = 10) {
-  await act(async () => {
-    _render(component);
-    await sleep(time);
-  });
-}
-
-function asssertContainerContent(value: string) {
-  expect(container.textContent).toBe(value);
-}
-
 it('returns  null on mount', async (done) => {
   expect.assertions(1);
   const store = stores[0];
   await act(async () => {
-    _render(<ReadComponent storeName={store.name} />);
-    asssertContainerContent(nullMsg);
+    const result = await renderHook(store.name, undefined, 0);
+    expect(result.current).toStrictEqual(createResult(null));
     await sleep(10);
   });
   done();
@@ -78,7 +72,7 @@ it('subscribes listener to store', async (done) => {
   expect.assertions(3);
   const store = stores[1];
   expect(Store['subscriptions'][store.name]).toBeUndefined();
-  await renderAndWait(<ReadComponent storeName={store.name} />);
+  await renderHook(store.name);
   expect(Store['subscriptions'][store.name]).toBeDefined();
   expect(Store['subscriptions'][store.name].listeners.length).toBe(1);
   done();
@@ -87,7 +81,7 @@ it('subscribes listener to store', async (done) => {
 it('unsubscribes listener after unmount', async (done) => {
   expect.assertions(2);
   const store = stores[1];
-  await renderAndWait(<ReadComponent storeName={store.name} />);
+  await renderHook(store.name);
   expect(Store['subscriptions'][store.name].listeners.length).toBe(1);
   unmountComponentAtNode(container);
   await sleep(10);
@@ -98,8 +92,8 @@ it('unsubscribes listener after unmount', async (done) => {
 it('returns store data', async (done) => {
   expect.assertions(1);
   const store = stores[1];
-  await renderAndWait(<ReadComponent storeName={store.name} />, 100);
-  asssertContainerContent(JSON.stringify(store.data));
+  const result = await renderHook(store.name);
+  expect(result.current).toStrictEqual(createResult(store.data));
   done();
 });
 
@@ -109,12 +103,12 @@ it('returns new store data on update', async (done) => {
   const newValue = 'new';
   const newData = [...store.data, newValue];
   let updateKey;
-  await renderAndWait(<ReadComponent storeName={store.name} />);
+  const result = await renderHook(store.name);
   await act(async () => {
     updateKey = await update(store.name, { value: newValue });
     await sleep(10);
   });
-  asssertContainerContent(JSON.stringify(newData));
+  expect(result.current).toStrictEqual(createResult(newData));
   unmountComponentAtNode(container);
   await sleep(10);
   await update(store.name, { value: null, key: updateKey });
@@ -124,30 +118,25 @@ it('returns new store data on update', async (done) => {
 it('returns new store data on param change', async (done) => {
   expect.assertions(2);
   const store = stores[0];
-  await renderAndWait(<ReadComponent storeName={store.name} />, 500);
-  asssertContainerContent(JSON.stringify(store.data));
-  await renderAndWait(
-    <ReadComponent storeName={store.name} params={{ direction: 'prev' }} />,
-    500,
-  );
-  asssertContainerContent(JSON.stringify([...store.data].reverse()));
+  let result = await renderHook(store.name);
+  expect(result.current).toStrictEqual(createResult(store.data));
+  result = await renderHook(store.name, { direction: 'prev' });
+  expect(result.current).toStrictEqual(createResult([...store.data].reverse()));
   done();
 });
 
 it('errors on unknown store', async (done) => {
   expect.assertions(1);
-  await renderAndWait(<ReadComponent storeName={'unknown'} />);
-  asssertContainerContent(errorMsg);
+  const result = await renderHook('unknown');
+  expect(result.current[1]).toBeTruthy();
   done();
 });
 
 it('returns data in prev direction', async (done) => {
   const store = stores[1];
   expect.assertions(1);
-  await renderAndWait(
-    <ReadComponent storeName={store.name} params={{ direction: 'prev' }} />,
-  );
-  asssertContainerContent(JSON.stringify([...store.data].reverse()));
+  const result = await renderHook(store.name, { direction: 'prev' });
+  expect(result.current).toStrictEqual(createResult([...store.data].reverse()));
   done();
 });
 
@@ -155,8 +144,8 @@ it('returns filtered data', async (done) => {
   const store = stores[0];
   expect.assertions(1);
   const filter = (i: unknown) => i !== store.data[0];
-  await renderAndWait(<ReadComponent storeName={store.name} params={{ filter }} />);
-  asssertContainerContent(JSON.stringify([...store.data].filter(filter)));
+  const result = await renderHook(store.name, { filter });
+  expect(result.current).toStrictEqual(createResult([...store.data].filter(filter)));
   done();
 });
 
@@ -164,71 +153,54 @@ it('returns filtered data in prev direction', async (done) => {
   const store = stores[0];
   expect.assertions(1);
   const filter = (i: unknown) => i !== store.data[0];
-  await renderAndWait(
-    <ReadComponent storeName={store.name} params={{ filter, direction: 'prev' }} />,
+  const result = await renderHook(store.name, { filter, direction: 'prev' });
+  expect(result.current).toStrictEqual(
+    createResult([...store.data].filter(filter).reverse()),
   );
-  asssertContainerContent(JSON.stringify([...store.data].filter(filter).reverse()));
   done();
 });
 
 it('returns one item by key', async (done) => {
   const store = stores[1];
   expect.assertions(1);
-  await renderAndWait(
-    <ReadComponent
-      storeName={store.name}
-      params={{ direction: 'prev', key: store.data[0][keyPath] }}
-    />,
-  );
-  asssertContainerContent(JSON.stringify(store.data[0]));
+  const result = await renderHook(store.name, {
+    direction: 'prev',
+    key: store.data[0][keyPath],
+  });
+  expect(result.current).toStrictEqual(createResult(store.data[0]));
   done();
 });
 
 it('returns one item by multiple keys', async (done) => {
   const store = stores[2];
   expect.assertions(1);
-  await renderAndWait(
-    <ReadComponent
-      storeName={store.name}
-      params={{
-        direction: 'prev',
-        key: [store.data[0][keyPath], store.data[0][keyPath2]],
-      }}
-    />,
-  );
-  asssertContainerContent(JSON.stringify(store.data[0]));
+  const result = await renderHook(store.name, {
+    direction: 'prev',
+    key: [store.data[0][keyPath], store.data[0][keyPath2]],
+  });
+  expect(result.current).toStrictEqual(createResult(store.data[0]));
   done();
 });
 
 it('returns null on non-existent key', async (done) => {
   const store = stores[1];
   expect.assertions(1);
-  await renderAndWait(
-    <ReadComponent
-      storeName={store.name}
-      params={{
-        direction: 'prev',
-        key: 'unknown',
-      }}
-    />,
-  );
-  asssertContainerContent(nullMsg);
+  const result = await renderHook(store.name, {
+    direction: 'prev',
+    key: 'unknown',
+  });
+  expect(result.current).toStrictEqual(createResult(null));
   done();
 });
 
 it('returns null on non-existent key combination', async (done) => {
   const store = stores[2];
   expect.assertions(1);
-  await renderAndWait(
-    <ReadComponent
-      storeName={store.name}
-      params={{
-        direction: 'prev',
-        key: [store.data[0][keyPath], store.data[1][keyPath2]],
-      }}
-    />,
-  );
-  asssertContainerContent(nullMsg);
+  const result = await renderHook(store.name, {
+    direction: 'prev',
+    key: [store.data[0][keyPath], store.data[1][keyPath2]],
+  });
+  expect(result.current).toStrictEqual(createResult(null));
   done();
 });
 
@@ -236,16 +208,11 @@ it('returns key-bound items ', async (done) => {
   const store = stores[1];
   const [lower, upper] = [1, 3];
   expect.assertions(1);
-  await renderAndWait(
-    <ReadComponent
-      storeName={store.name}
-      params={{
-        keyRange: IDBKeyRange.bound(lower, upper),
-      }}
-    />,
-  );
-  asssertContainerContent(
-    JSON.stringify(
+  const result = await renderHook(store.name, {
+    keyRange: IDBKeyRange.bound(lower, upper),
+  });
+  expect(result.current).toStrictEqual(
+    createResult(
       (store.data as any[])
         .filter(({ key }) => key >= lower)
         .filter(({ key }) => key <= upper),
@@ -254,38 +221,28 @@ it('returns key-bound items ', async (done) => {
   done();
 });
 
-it('returns lower key-bound items ', async (done) => {
+it('returns upper-key-bound items ', async (done) => {
   const store = stores[1];
-  const [lower] = [1];
+  const [, upper] = [1, 3];
   expect.assertions(1);
-  await renderAndWait(
-    <ReadComponent
-      storeName={store.name}
-      params={{
-        keyRange: IDBKeyRange.lowerBound(lower),
-      }}
-    />,
-  );
-  asssertContainerContent(
-    JSON.stringify((store.data as any[]).filter(({ key }) => key >= lower)),
+  const result = await renderHook(store.name, {
+    keyRange: IDBKeyRange.upperBound(upper),
+  });
+  expect(result.current).toStrictEqual(
+    createResult((store.data as any[]).filter(({ key }) => key <= upper)),
   );
   done();
 });
 
-it('returns upper key-bound items ', async (done) => {
+it('returns lower-key-bound items ', async (done) => {
   const store = stores[1];
-  const [upper] = [1];
+  const [lower] = [1, 3];
   expect.assertions(1);
-  await renderAndWait(
-    <ReadComponent
-      storeName={store.name}
-      params={{
-        keyRange: IDBKeyRange.upperBound(upper),
-      }}
-    />,
-  );
-  asssertContainerContent(
-    JSON.stringify((store.data as any[]).filter(({ key }) => key <= upper)),
+  const result = await renderHook(store.name, {
+    keyRange: IDBKeyRange.lowerBound(lower),
+  });
+  expect(result.current).toStrictEqual(
+    createResult((store.data as any[]).filter(({ key }) => key >= lower)),
   );
   done();
 });
